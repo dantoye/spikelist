04f6752a979deb4fd7bc1a303290b4f2607b869a
Dan Toye <dan.toye@clixxa.com>
2019-11-20 20:33:21 -0800

    flesh out more of the structure

--- main.go
+++ main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func getLevels(req events.APIGatewayProxyRequest) (res events.APIGatewayProxyResponse, err error) {
-	res.Body = "hello world"
+func upload(eq events.APIGatewayProxyRequest) (res events.APIGatewayProxyResponse, err error) {
+	res.Body = fmt.Sprintf("%s:%s", eq.QueryStringParameters["text"], eq.Headers["Nightbot-User"])
 	res.StatusCode = 200
+	log.Printf("responding with %s", res.Body)
+	return
+}
+
+func rate(eq events.APIGatewayProxyRequest) (res events.APIGatewayProxyResponse, err error) {
+	res.Body = fmt.Sprintf("%s:%s", eq.QueryStringParameters["text"], eq.Headers["Nightbot-User"])
+	res.StatusCode = 200
+	log.Printf("responding with %s", res.Body)
+	return
+}
+
+func register(eq events.APIGatewayProxyRequest) (res events.APIGatewayProxyResponse, err error) {
+	res.Body = fmt.Sprintf("%s:%s", eq.QueryStringParameters["text"], eq.Headers["Nightbot-User"])
+	res.StatusCode = 200
+	log.Printf("responding with %s", res.Body)
 	return
 }
 
 func main() {
 	handlers := map[string]interface{}{
-		"get_levels": getLevels,
+		"upload":   upload,
+		"rate":     rate,
+		"register": register,
 	}
 	lambda.Start(handlers[os.Getenv("HANDLER_NAME")])
 }
